fb4bc44b3105a34d73484eada7cb251f2acfa245
David Brizendine <d_briz@hotmail.com>
2020-07-02 16:51:38 -0500

    Revision 1

--- intro.py
+++ intro.py
@@ -1,5 +1,8 @@
 import os
 import sys
+
+import requests
+
 print(sys.version)
 print(sys.executable)
 
@@ -7,3 +10,7 @@ print(sys.executable)
 print(os.getcwd())
 
 print(os.__file__)
+
+r = requests.get('www.google.com')
+print(r.status_code)
+print(r.ok)
